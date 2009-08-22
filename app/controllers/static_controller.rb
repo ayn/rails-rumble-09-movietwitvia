576b84b3be8e7cf50049095bbd8ffa6668b0d5d0
Andrew Ng <ayn@andrewng.com>
2009-08-22 15:12:13 -0700

    hooked up leaderboard

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,6 @@
 class StaticController < ApplicationController
   def index
     @users = User.all(:order => "created_at DESC", :limit => 16)
+    @leaders = User.leaderboard(params[:time_period] || 'all-time')
   end
 end
