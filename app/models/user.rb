1c47212b9634052f67d5ecf22c97db3215819eba
Andrew Ng <ayn@andrewng.com>
2009-08-21 20:29:49 -0700

    user has many winnings

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < TwitterAuth::GenericUser
   # Extend and define your user model as you see fit.
   # All of the authentication logic is handled by the 
   # parent TwitterAuth::GenericUser class.
+  
+  has_many :winnings, :class_name => :questions, :foreign_key => 'winner_id'
 end
